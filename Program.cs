4731b1221a8db75b4102a4109feb1638be63cd3d
Clone888 <dwclone@gmail.com>
2023-09-22 13:35:00 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+// DEL 2!!!!!!!!!!!
+
+var floor = 5;
+
+if (floor % 10 == 4 || floor % 100 == 13)
+{
+    Console.WriteLine("Kan ej omvandla till riktigt våningsnummer");
+}
+
+else
+{
+    if (floor == 5)     //Funkar inte på enkel "5" då blir det "4" vill ha det till "3"??
+    {              
+        Console.WriteLine(floor - 1);
+             
+        if (floor % 10 == 4)
+        {
+            Console.WriteLine(floor - 2);
+        }
+
+    }
+    else if (floor % 10 == 5 || floor % 100 == 15)
+    {
+        Console.WriteLine(floor - 3);
+    }
+    else
+    {
+        Console.WriteLine(floor - 1);
+    }
+}
